4cbe17c7d8d3a157efe6d688728eea7b89c563fc
gilsonfonseca92@gmail.com <gilson.silva@finanblue.com.vc>
2025-03-20 13:45:09 -0300

    Adicionando mais dados

--- src/pages/Programa/Programa.js
+++ src/pages/Programa/Programa.js
@@ -3,6 +3,18 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Programa.css";
+import pascoa from "../images/pascoa.jpeg";
+import pascoa01 from "../images/pascoa01.jpeg";
+import pascoa02 from "../images/pascoa02.jpeg";
+import pascoa03 from "../images/pascoa03.jpeg";
+import natal from "../images/natal.jpeg";
+import natalOne from "../images/natal02.jpeg";
+import natalSeg from "../images/natal03.jpeg";
+import natalTerc from "../images/natal04.jpeg";
+
+
+
+
 
 const News = ({ title, date, content, category, images }) => {
   const settings = {
@@ -39,44 +51,32 @@ const HolidayNews = () => {
       date: "31 de março de 2024",
       content: "A Páscoa está chegando! Tempo de renovação, esperança e união entre amigos e familiares. Celebre com amor e alegria!",
       category: "Notícias",
-      images: [
-        "https://via.placeholder.com/300/FF5733",
-        "https://via.placeholder.com/300/33FF57",
-        "https://via.placeholder.com/300/3357FF"
-      ]
+      images: [pascoa, pascoa01, pascoa02, pascoa03]
     },
     {
       title: "Feliz Natal!",
       date: "25 de dezembro de 2024",
       content: "O Natal chegou! Um momento especial para compartilhar felicidade e espalhar o espírito natalino. Desejamos a todos um Natal cheio de amor e paz!",
       category: "Artigo",
-      images: [
-        "https://via.placeholder.com/300/FF3333",
-        "https://via.placeholder.com/300/33FFFF",
-        "https://via.placeholder.com/300/FFFF33"
-      ]
+      images: [natal,natalOne,natalSeg,natalTerc]
     },
+
     {
       title: "Dia das Crianças Chegando!",
       date: "12 de outubro de 2024",
       content: "Dia de celebrar a alegria e a inocência das crianças! Diversas atividades especiais acontecerão para garantir a diversão dos pequenos!",
       category: "Notícias",
       images: [
-        "https://via.placeholder.com/300/9933FF",
-        "https://via.placeholder.com/300/FF9933",
-        "https://via.placeholder.com/300/99FF33"
+        "/images/QrCode02.jpeg",
+        "/images/alimentacaoSolidaria.jpg",
+        "/images/batalhaRap.webp"
       ]
     }
   ];
 
   return (
     <div className="news-container">
-      <h1 className="blog-title">Ações realizadas
-      </h1>
-      <div className="news-filters">
-        
-       
-      </div>
+      <h1 className="blog-title">Ações realizadas</h1>
       <div className="news-grid">
         {newsList.map((news, index) => (
           <News key={index} {...news} />
@@ -85,5 +85,6 @@ const HolidayNews = () => {
     </div>
   );
 };
+
 
 export default HolidayNews;
